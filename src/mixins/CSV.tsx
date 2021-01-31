e6f8ddcc3907fcf4d22a8e7b994a6b644abb8c57
Kostia (Kildor) Romanov <kromanov@gmail.com>
2021-01-31 10:07:43 +0700

    * allow to download the CSV file

--- src/mixins/CSV.tsx
+++ src/mixins/CSV.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react'
+import I18n from '../classes/I18n';
 import CSVConvertInterface from '../interfaces/CSVConvertInterface';
+import FileSaver from 'file-saver'
 
 export interface CSVProps {
 	header: Function
@@ -7,10 +9,17 @@ export interface CSVProps {
 	children: Array<CSVConvertInterface>
 };
 
-export default ({header, children, useRank=true} : CSVProps) : ReactElement => {
+const download = (csv: string, filename?: string)=>{
+	const blob = new Blob([csv], {type: 'text/csv'});
+	FileSaver.saveAs(blob, filename || "stats.csv");
+}
+export default ({ header, children, useRank = true }: CSVProps): ReactElement => {
 	let value = header(useRank) +
-		children.map((element : CSVConvertInterface, index : number) => element.toCSV(useRank ? index : false)).join("\n");
-	 return (
-	 <textarea value={value} readOnly style={{ width: "700px", maxWidth: "90vw", height: "200px" }} onFocus={(e) => { e.target.select() }} />
-	 )
+		children.map((element: CSVConvertInterface, index: number) => element.toCSV(useRank ? index : false)).join("\n");
+	return (
+		<div className="csv-wrapper" style={{ width: "700px", maxWidth: "90vw" }}>
+			<textarea value={value} readOnly style={{ width: "100%", height: "200px", }} onFocus={(e) => { e.target.select() }} />
+			<button style={{display:"block", margin:"5px auto"}} className="button button-download" onClick={e=>{download(value)}}>{I18n.t("Download")}</button>
+		</div>
+	)
 }
