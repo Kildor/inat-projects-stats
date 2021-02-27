64865600b01260b3f5f9616a487a515854b422b0
Kostia (Kildor) Romanov <kromanov@gmail.com>
2021-02-27 13:10:48 +0700

    * geojson could be null

--- src/DataObjects/Observation.ts
+++ src/DataObjects/Observation.ts
@@ -18,15 +18,15 @@ class Observation implements CSVConvertInterface {
 	commonName: string | null;
 	taxon: Taxon;
 	user: User;
-	coordinates: Array<number>
-	location: string|null|undefined
-	quality_grade: string
-	geoprivacy: string
+	coordinates: Array<number> | null;
+	location: string|null|undefined;
+	quality_grade: string;
+	geoprivacy: string;
 	activity: Array<ObservationComment | ObservationIdentification>;
 	toCSV(onlyObservations = false ) {
 		let str = '';	
 		str += `${this.id}\t"${this.name}"\t${!!this.commonName ? '"' + this.commonName + '"' : ''}\t${this.quality_grade}\t${this.created.toLocaleString()}\t`
-		+`${this.coordinates.toString()}${this.geoprivacy===null?'':' ('+this.geoprivacy+')'}\t"${this.location}"\t${this.user.fullName}`;
+			+ `${!!this.coordinates ? this.coordinates.toString():'coordinates missed'}${this.geoprivacy===null?'':' ('+this.geoprivacy+')'}\t"${this.location}"\t${this.user.fullName}`;
 		if (onlyObservations!==false) this.activity.forEach(activity=>{
 			str+=activity.toCSV();
 		})
@@ -39,7 +39,7 @@ class Observation implements CSVConvertInterface {
 		this.user = new User(jsonObservation.user);
 		this.name = jsonObservation.taxon.name;
 		this.commonName = jsonObservation.taxon.preferred_common_name;
-		this.coordinates = jsonObservation.geojson.coordinates;
+		this.coordinates = !! !!jsonObservation.geojson ? jsonObservation.geojson.coordinates : null;
 		this.location = jsonObservation.place_guess;
 		this.quality_grade = jsonObservation.quality_grade;
 		this.geoprivacy = jsonObservation.geoprivacy;
